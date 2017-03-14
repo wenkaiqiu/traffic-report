cdfbc8ec1a90c11aea2acc3ee7aa152368223262
Qiu Wenkai <chlwqwk@163.com>
2017-03-15 00:47:06 +0800

    feat(component): init components
    Analyzation-FlowTrend.vue
    Analyzation-RealTime.vue
    Management-Basic.vue
    Management-Configuration.vue
    Recall-DataPackage.vue
    Recall-HistoryFlow.vue
    Greeting.vue

---
+++ src/components/Analyzation-FlowTrend.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="flow-trend">
+    <h1>Flow Trend Analyzation</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'flow-trend',
+  };
+</script>
+
+<style rel="stylesheet/sass" lang="sass">
+  #flow-trend
+
+</style>
---
+++ src/components/Recall-DataPackage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="data-package">
+    <h1>Data Package Storage and Index</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'data-package',
+  };
+</script>
+
+<style rel="stylesheet/sass" lang="sass">
+  #data-package
+
+</style>
