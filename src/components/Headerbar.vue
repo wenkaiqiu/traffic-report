3540c29c43b1ee136bdaeb7f2232c7b50b3cebfd
Qiu Wenkai <chlwqwk@163.com>
2017-03-14 20:51:32 +0800

    feat(component): init Headerbar component and Menubar component

---
+++ src/components/Headerbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="headerbar">
+    headerbar
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'headerbar',
+  };
+</script>
+
+<style rel="stylesheet/sass" lang="sass">
+  #headerbar
+
+</style>
---
+++ src/components/Menubar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="menubar">
+    menubar
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'menubar',
+  };
+</script>
+
+<style rel="stylesheet/sass" lang="sass">
+  #menubar
+
+</style>
