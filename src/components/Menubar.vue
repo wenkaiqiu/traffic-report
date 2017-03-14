b85c00288379c934e657c268571269dd072f7f6d
Qiu Wenkai <chlwqwk@163.com>
2017-03-15 01:09:18 +0800

    feat(router): 在menubar添加各页面路由链接，并测试

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="menubar">
-    
+    <ul>
+      <router-link tag="li" to="/flow-trend">
+        <a>FlowTrend</a>
+      </router-link>
+      <router-link tag="li" to="/real-time">
+        <a>RealTime</a>
+      </router-link>
+      <router-link tag="li" to="/basic-management">
+        <a>BasicManagement</a>
+      </router-link>
+      <router-link tag="li" to="/configuration-management">
+        <a>ConfigurationManagement</a>
+      </router-link>
+      <router-link tag="li" to="/data-package">
+        <a>DataPackage</a>
+      </router-link>
+      <router-link tag="li" to="/history-flow">
+        <a>HistoryFlow</a>
+      </router-link>
+    </ul>
   </div>
 </template>
 
