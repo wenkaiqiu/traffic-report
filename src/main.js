4b754487100885d056f0e28c8f06747597ec3ed9
Qiu Wenkai <chlwqwk@163.com>
2017-03-16 15:05:26 +0800

    feat(component): 在main.js中调用style文件，并完成Header组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap';
+import '../static/css/material-design-iconic-font.min.css';
+import '../static/css/font-awesome.min.css';
+import '../static/css/global.sass';
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
