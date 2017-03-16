80e312d2cb1d5a55ade1e62bb9468d1dc7693301
Qiu Wenkai <chlwqwk@163.com>
2017-03-16 15:23:36 +0800

    feat(plugin): 使用Vuex，并将其单独组织在一个文件夹里

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 import router from './router';
+import store from './stores';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap';
 import '../static/css/material-design-iconic-font.min.css';
 import '../static/css/font-awesome.min.css';
 import '../static/css/global.sass';
 
-Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App },
